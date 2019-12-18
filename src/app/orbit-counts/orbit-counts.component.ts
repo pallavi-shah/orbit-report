32839b4450f018a2885784a8736494b6e7e50e33
Pallavi Shah <emailpallavishah@gmail.com>
2019-12-17 23:08:36 -0600

    Studio 3 completed

---
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit,Input } from '@angular/core';
+import { Satellite } from '../satellite';
+
+@Component({
+  selector: 'app-orbit-counts',
+  templateUrl: './orbit-counts.component.html',
+  styleUrls: ['./orbit-counts.component.css']
+})
+export class OrbitCountsComponent implements OnInit {
+  @Input() satellites: Satellite[];
+   
+  constructor() { 
+  }
+  
+  countSatellite(name: string){
+    let count=0;
+
+    for(let i=0;i<this.satellites.length;i++){
+      if (this.satellites[i].type === name)  {
+        count=count+1;
+      }     
+      
+      
+    }
+     return count;
+  }
+  
+  ngOnInit() {
+  }
+
+}
